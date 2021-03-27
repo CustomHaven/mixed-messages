1109cd2089cab469f9630aa6adafedd0909fcc53
Mohamed <marvellouslegs@gmail.com>
2021-03-27 18:12:21 +0000

    a lot more random emojis added 56more!

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // Generating a Random Number
-const generateRandomNum = arraylen => Math.floor(Math.random() * arraylen);
+const generateRandomNum = num => Math.floor(Math.random() * num);
 
 // empty array to push the message to
-
 const newArray = [];
 
 // Mixed Messages saved to an Object
@@ -16,9 +15,8 @@ const programmingLanguage = {
 };
 
 
-/*
-
-'⁏	204F'
+/* Below part helps me visualize the ASCII Hex Codes: commented out as it is just a test of how it should look on completion
+'⁏	204F' reversed semicolon
 'Front-End Engineer ⁏⊂(◉‿◎)つ'
 '‿	203F'
 '◉	25C9'
@@ -41,9 +39,7 @@ const programmingLanguage = {
 */
 
 
-
-
-// Itirating over the Object
+// Itirating over the Object then populating the empty newArray with help off switch statement.
 for (let key in programmingLanguage) {
 
     let indexGen = generateRandomNum(programmingLanguage[key].length)
@@ -66,16 +62,27 @@ for (let key in programmingLanguage) {
     }
 };
 
-const valStr = newArray[3];
 
-// converting the array to a string
-const stringify = (array,val) => {
-
-    console.log("emiii " + String.fromCodePoint(parseInt("1F923", 16)))
-
+// converting the array to a string and sending a random emoji for the laughs
+const stringify = (array, emojiArray) => {
+    
+    // ASCII and Emoji unicodes has been included for the project
+    array.unshift(`We kick off with a Random Emoji: "${String.fromCodePoint(parseInt(emojiArray[generateRandomNum(emojiArray.length)], 16))}".`);
     console.log(array.join("\n"));
-
 };
 
-console.log("\u{21AD}"); 
-stringify(newArray, valStr);
+
+// Array of 56 different Emojis
+const emojiArray = [
+"1F600", "1F601", "1F602", "1F603", "1F604", "1F605", "1F606", "1F607", 
+"1F608", "1F609", "1F60A", "1F60B", "1F60C", "1F60D", "1F60E", "1F60F",
+"1F610", "1F611", "1F612", "1F613", "1F614", "1F615", "1F616", "1F617",
+"1F618", "1F619", "1F61A", "1F61B", "1F61C", "1F61D", "1F61E", "1F61F",
+"1F620", "1F621", "1F622", "1F623", "1F624", "1F625", "1F626", "1F627",
+"1F628", "1F629", "1F62A", "1F62B", "1F62C", "1F62D", "1F62E", "1F62F",
+"1F630", "1F631", "1F632", "1F633", "1F634", "1F635", "1F636", "1F637"
+]
+
+
+// Final Part calling the project function which will give the output as a string
+stringify(newArray, emojiArray);
